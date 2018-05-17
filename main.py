dad19c63759b5ac835da206d5672a3184ef41f7b
Jayden Chen <chenzhenhaojayden@gmail.com>
2018-05-17 14:18:15 +0800

    change static url path

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, request, render_template
 from config import *
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/pycourse/static")
 
 def examinate_func(f):
     failed = []
