fd3fdb7bf888a955d6cf4156c32430e44cd82c36
Jayden Chen <chenzhenhaojayden@gmail.com>
2018-05-20 16:03:51 +0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from importlib import import_module
 from flask import Flask, request, render_template
 
@@ -7,13 +8,50 @@ from config import *
 
 app = Flask(__name__, static_url_path="/pycourse/static")
 
-def examinate_func(f):
-    failed = []
-    for params, output in TESTCASE:
+def examinate_func(f, test_cases):
+    errors = []
+    for params, expected in test_cases:
         result = f(**params)
-        if not (len(result) == len(output) and all([result[i] == o for i, o in enumerate(output)])):
-            failed.append([params, output, result])
-    return failed
+        if isinstance(result, tuple):
+            if not (len(result) == len(expected) and all([result[i] == o for i, o in enumerate(expected)])):
+                errors.append((params, expected, result))
+        else:
+            if not result == expected[0]:
+                errors.append((params, expected, (result, )))
+    return {"status": WRONG_RETURN, "errors": errors} if errors else {"status": PASSED}
+
+
+def check_func(f, checker):
+    errors = []
+    check_f = checker["func"]
+    for params in checker["cases"]:
+        code, msg = check_f(f, **params)
+        if code == CHECK_FAILED:
+            errors.append((params, msg))
+    return {"status": CHECK_FAILED, "errors": errors} if errors else {"status": PASSED}
+
+
+def format_func_call(func_name, params):
+    param_s = ", ".join(["{}={}".format(k, v) for k, v in params.items()])
+    return "{}({})".format(func_name, param_s)
+
+
+def format_error(func_name, error_info):
+    status = error_info["status"]
+    if status == WRONG_RETURN:
+        formated_errors = []
+        for params, expected, result in error_info["errors"]:
+            func_call = format_func_call(func_name, params)
+            expected_s = ", ".join((str(e) for e in expected))
+            result_s = ", ".join((str(r) for r in result))
+            formated_errors.append((func_call, expected_s, result_s))
+        error_info["errors"] = formated_errors
+    elif status == CHECK_FAILED:
+        formated_errors = []
+        for params, msg in error_info["errors"]:
+            func_call = format_func_call(func_name, params)
+            formated_errors.append((func_call, msg))
+        error_info["errors"] = formated_errors
 
 
 @app.route("/pycourse/assignment8", methods=["GET", "POST"])
@@ -23,18 +61,28 @@ def examinate_assignment8():
     else:
         name = request.form["name"]
         upload_file = request.files['assignment']
-        upload_file.save(ASSIGNMENT_DIR)
+        filename = upload_file.filename
+        upload_file.save(os.path.join(ASSIGNMENT_DIR, filename))
+        assignment = import_module("assignment8.{}".format(filename).replace(".py", ""))
 
-        assignment = import_module(upload_file.filename, package="assignment8")
-        func = getattr(assignment, FUNCTION_NAME)
-        if func:
-            failed = examinate_func(func)
-            if failed:
-                pass
+        passed = True
+        result = []
+        for func_name in FUNCTION_NAMES:
+            func = getattr(assignment, func_name)
+            if func:
+                if func_name in TESTCASE:
+                    info = examinate_func(func, TESTCASE[func_name])
+                else:
+                    info = check_func(func, CHECKERS[func_name])
+
+                if info["status"] != PASSED:
+                    format_error(func_name, info)
+                    passed = False
+                result.append(info)
             else:
-                pass
-        else:
-            pass
+                result.append({"status": NOT_FOUND})
+        return render_template("result.html", name=name, result=result, passed=passed, enumerate=enumerate,
+                               status=STATUS)
 
 
 if __name__ == '__main__':
